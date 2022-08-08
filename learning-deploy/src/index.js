9aa6f4b970dd8dc32266e64c8d4797f2748657ff
azoom-nguyen-van-nam <nguyen.van.nam@azoom.jp>
2022-08-08 07:21:48 +0000

    :+1: Add cloudbuild.yaml

--- learning-deploy/src/index.js
+++ learning-deploy/src/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  console.log('oke');
+  console.log(process.env);
   res.send('Here we go!');
 });
 app.listen(process.env.PORT || 1337, function () {
-  console.log('Example app listening on port 1337!');
+  console.log(`Example app listening on port ${process.env.PORT || 1337}!`);
 });
